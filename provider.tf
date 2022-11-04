32f062ab088b39e7f676c7edab19abb7d8ddbc8e
AbeYuki <aim4highergg@gmail.com>
2022-11-04 11:47:39 +0900

    Modify node

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "helm" {
   kubernetes {
-    config_path    = "~/.kube/config_microk8s_node1"
-    config_context = "microk8s"
+    config_path    = "~/.kube/config_microk8s_node2"
+    config_context = "microk8s-node2"
   }
 }
