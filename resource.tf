6076ae05588de61155fe222a33f241b994d8c012
AbeYuki <aim4highergg@gmail.com>
2022-11-05 19:02:47 +0900

    modify version

--- resource.tf
+++ resource.tf
@@ -5,7 +5,7 @@ resource "helm_release" "argocd" {
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
   namespace        = "argocd"
-  version          = "4.13.2"
+  version          = "4.10.9"
   create_namespace = true
 
   set {
