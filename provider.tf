e70ef24289b9a529547da15712638f28536c2eeb
AbeYuki <aim4highergg@gmail.com>
2023-01-26 01:46:19 +0900

    Add provider backend

--- provider.tf
+++ provider.tf
@@ -1,4 +1,10 @@
 provider "helm" {
+  backend "kubernetes" {
+    secret_suffix  = "onpremises-helm-argocd"
+    config_path    = "~/.kube/config_microk8s_node1"
+    namespace      = "terraform"
+    config_context = "microk8s-node1"
+  }
   kubernetes {
     config_path    = "~/.kube/config_microk8s_node2"
     config_context = "microk8s-node2"
