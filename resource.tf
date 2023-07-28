41e490cf9a49c0cbb986affb47767161723a813c
AbeYuki <aim4highergg@gmail.com>
2023-07-28 20:47:03 +0900

    update argcod-vault-plaguin

--- resource.tf
+++ resource.tf
@@ -5,11 +5,19 @@ resource "helm_release" "argocd" {
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
   namespace        = "argocd"
-  version          = "5.25.0"
+  version          = "5.41.1"
   create_namespace = true
 
   set {
     name  = "server.service.type"
     value = "NodePort"
+  }
+  values = [
+    file("${path.module}/values.yaml")
+  ]
+resource "kubernetes_secret" "argocd_vault_plugin" {
+  metadata {
+    name      = "argocd-vault-plugin-credentials"
+    namespace = "argocd"
   }
 }
