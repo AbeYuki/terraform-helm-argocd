53db74710924541c51cb9527e47d088c3b0ca2ea
AbeYuki <aim4highergg@gmail.com>
2024-09-03 20:46:43 +0900

    update 7.5.2

--- modules/common/resource.tf
+++ modules/common/resource.tf
@@ -31,7 +31,7 @@ resource "helm_release" "argocd" {
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
   namespace        = "argocd"
-  version          = "7.4.2"
+  version          = "7.5.2"
   create_namespace = true
   set {
     name  = "server.service.type"
